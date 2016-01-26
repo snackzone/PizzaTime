d6d8ea507ed3de7d1b9a043e9a6f3d4dd7304545
snackzone <zachary.moroni@gmail.com>
2016-01-26 10:20:37 -0500

    dev gems

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  address     :string           not null
+#  lat         :float            not null
+#  lng         :float            not null
+#  price_range :integer          not null
+#  url         :string
+#  photo_url   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Restaurant < ActiveRecord::Base
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  address     :string           not null
+#  lat         :float            not null
+#  lng         :float            not null
+#  price_range :integer          not null
+#  url         :string
+#  photo_url   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
