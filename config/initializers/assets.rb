5f008d13491eaadf10febbcb43a590dad0903271
App Academy Student <appacademy@Apps-Mac-mini-53.local>
2016-01-27 13:14:40 -0500

    Add map markers to script in views and precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( map_marker_solid.png )
+
+(1..10).each do |n|
+  Rails.application.config.assets.precompile += [ "#{n}_black.png"]
+  Rails.application.config.assets.precompile += [ "#{n}_red.png"]
+end
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 # Precompile additional assets.
