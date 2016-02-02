9cc177c98d98b8b4139d1eba4780857dce0bba2e
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-02 08:38:06 -0500

    Add stars to map search index.

--- app/views/api/restaurants/index.json.jbuilder
+++ app/views/api/restaurants/index.json.jbuilder
@@ -1,10 +1,3 @@
 json.array! @restaurants do |restaurant|
-  json.name restaurant.name
-  json.address restaurant.address
-  json.url restaurant.url
-  json.lat restaurant.lat
-  json.lng restaurant.lng
-  json.price_range restaurant.price_range
-  json.id restaurant.id
-  json.photo_url restaurant.photo_url
+  json.partial! "api/restaurants/restaurant", restaurant: restaurant
 end
--- frontend/components/search/index_item.jsx
+++ frontend/components/search/index_item.jsx
@@ -4,6 +4,11 @@ var RestaurantActions = require('../../actions/restaurant_actions');
 var RestaurantStore = require('../../stores/restaurant_store');
 
 var IndexItem = React.createClass({
+  getStars: function () {
+    var rating = this.props.restaurant.mean_rating;
+    return window.PizzaTime.imageUrls.stars[rating - 1];
+  },
+
   render: function () {
     var restaurant = this.props.restaurant;
     var klass = "search-index-item group";
@@ -16,22 +21,24 @@ var IndexItem = React.createClass({
     var num = (RestaurantStore.findIndexById(restaurant.id) + 1).toString();
     return(
       <li className={klass}
-          id={restaurant.id}
-          onMouseOver={RestaurantActions.focusRestaurant}
-          onMouseLeave={RestaurantActions.unfocusAllRestaurants}>
+        id={restaurant.id}
+        onMouseOver={RestaurantActions.focusRestaurant}
+        onMouseLeave={RestaurantActions.unfocusAllRestaurants}>
 
         <Link to={"/restaurants/" + restaurant.id}>
           <img src={restaurant.photo_url}
-               className="restaurant-thumb"/>
+            className="restaurant-thumb"/>
         </Link>
 
+
         <article className="search-index-item-info-container group">
-           <div className="num">{num}: </div>
-           <Link to={"/restaurants/" + restaurant.id}>
-             {restaurant.name}
-           </Link>
-           <p>{restaurant.address}</p>
-           <div>{priceRangeString}</div>
+          <div className="num">{num}: </div>
+          <Link to={"/restaurants/" + restaurant.id}>
+          {restaurant.name}
+          </Link>
+          <img className="stars" src={this.getStars()}/>
+          <p>{restaurant.address}</p>
+          <div>{priceRangeString}</div>
         </article>
 
       </li>
