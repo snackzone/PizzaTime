2efcbfdd9aaa4659e5a10a11aed1303ed0a739ad
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-02 21:59:36 -0500

    Finally finish styling form.

--- frontend/components/forms/review_form.jsx
+++ frontend/components/forms/review_form.jsx
@@ -9,6 +9,7 @@ var ReactRouter = require('react-router');
 var RestaurantApiUtil = require('../../util/restaurant_api_util');
 var RestaurantStore = require('../../stores/restaurant_store');
 var ReviewApiUtil = require('../../util/review_api_util');
+var ReviewIndex = require('../restaurants/restaurant_review_index');
 
 
 var ReviewForm = React.createClass({
@@ -110,41 +111,55 @@ var ReviewForm = React.createClass({
       );
     }
 
+    var disabled = (this.state.body.length < 20 ||
+      this.state.body.length > 300 ||
+      this.state.review < 1);
+
+    var buttonClass = "big-red-button submit-button";
+    if (disabled) {
+      buttonClass += " disabled";
+    }
+
     var restaurant = this.state.restaurant;
     var Link = ReactRouter.Link;
     return (
-      <div className="review-form new-review">
+      <div className="review-form new-review group">
         <h1>Write a Review</h1>
-
-        <div className="restaurant-info-mini-container">
-          <img src={restaurant.photo_url}/>
-          <Link to={"/restaurants/" + restaurant.id}>{restaurant.name}</Link>
-          <p>{this.getPriceRangeString()}</p>
-          <p>{restaurant.address}</p>
-        </div>
-
-        <form className="review-form group" onSubmit={this.handleSubmit}>
-
-
-          <div className="review-body">
-
-            <RatingSelector
-              rating={this.state.rating}
-              ratingSet={this.state.ratingSet}
-              handleClick={this.handleRating}
-            />
-
-            <textarea
-              valueLink={this.linkState('body')}
-              placeholder="Write your review here!">
-            </textarea>
-
-            <button className="big-red-button submit-button">Post Review</button>
-
+        <div className="new-review-left group">
+          <div className="restaurant-info-mini-container">
+            <img src={restaurant.photo_url}/>
+            <Link to={"/restaurants/" + restaurant.id}>{restaurant.name}</Link>
+            <p>{this.getPriceRangeString()}</p>
+            <p className="address">{restaurant.address}</p>
           </div>
 
-        </form>
-        {errors}
+          <form className="review-form group" onSubmit={this.handleSubmit}>
+
+
+            <div className="review-form-body">
+
+              <RatingSelector
+                rating={this.state.rating}
+                ratingSet={this.state.ratingSet}
+                handleClick={this.handleRating}
+              />
+
+              <textarea
+                valueLink={this.linkState('body')}
+                placeholder="Write your review here!">
+              </textarea>
+
+              <button disabled={disabled} className={buttonClass}>Post Review</button>
+
+            </div>
+
+          </form>
+          {errors}
+        </div>
+        <div className="new-review-right group">
+          <h3>See What Others Have Written</h3>
+          <ReviewIndex reviews={this.state.restaurant.reviews.slice(0, 3)}/>
+        </div>
       </div>
     );
   }
