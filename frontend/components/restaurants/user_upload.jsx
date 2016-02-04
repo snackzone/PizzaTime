8a86a1e6e283e3ea35519453e7369417b85c237d
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2016-02-04 12:43:04 -0500

    Style image gallery.

--- frontend/components/restaurants/user_upload.jsx
+++ frontend/components/restaurants/user_upload.jsx
@@ -3,9 +3,10 @@ var React = require('react');
 var UserUpload = React.createClass({
   render: function () {
     var upload = this.props.upload;
+
     return (
       <li>
-        <p className="upload-caption">{"\""+upload.caption+"\""}</p>
+        {upload.caption ? <p className="upload-caption">{"\""+upload.caption+"\""}</p> : null}
         <img src={upload.url} className="restaurant-photo"/>
       </li>
     );
