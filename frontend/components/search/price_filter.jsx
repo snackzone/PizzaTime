3e18819cbe91d1540f35cb03ae4accb208966f35
snackzone <zachary.moroni@gmail.com>
2016-02-04 18:25:21 -0500

    Filter had better UI but needs refactoring.

--- frontend/components/search/price_filter.jsx
+++ frontend/components/search/price_filter.jsx
@@ -8,14 +8,46 @@ var pricePoints = [
 ];
 
 var PriceFilter = React.createClass({
-  render: function () {
+  getInitialState: function () {
+    var filter = this;
     var buttons = pricePoints.map(function(string, idx) {
-      return <PriceFilterButton num={idx + 1} title={string} key={idx}/>;
+      return <PriceFilterButton num={idx + 1} title={string} id={idx} active={false} maxPrice={false} key={idx} handleClick={filter.handleButtonClick}/>;
     });
+
+    return ({buttons: buttons});
+  },
+
+
+  handleButtonClick: function (id, flag) {
+    var buttons;
+    var filter = this;
+    if (flag) {
+      buttons = this.emptyButtons();
+    } else {
+      buttons = pricePoints.map(function(string, idx) {
+        var active = idx <= id;
+        var maxPrice = idx === id;
+        return <PriceFilterButton num={idx + 1} title={string} id={idx} active={active} key={idx} maxPrice={maxPrice} handleClick={filter.handleButtonClick}/>;
+      });
+    }
+    this.setState({buttons: buttons});
+  },
+
+  emptyButtons: function () {
+    var filter = this;
+    var buttons = pricePoints.map(function(string, idx) {
+      return <PriceFilterButton num={idx + 1} title={string} id={idx} active={false} maxPrice={false} key={idx} handleClick={filter.handleButtonClick}/>;
+    });
+
+    return buttons;
+  },
+
+  render: function () {
+
 
     return(
       <div className="price-filter">
-        {buttons}
+        {this.state.buttons}
       </div>
     );
   }
--- frontend/components/search/price_filter_button.jsx
+++ frontend/components/search/price_filter_button.jsx
@@ -4,6 +4,7 @@ var FilterActions = require('../../actions/filter_actions');
 var PriceFilterButton = React.createClass({
   handleClick: function (e) {
     e.preventDefault();
+    this.props.handleClick(this.props.id, this.props.maxPrice);
     FilterActions.receiveFilter({max_price: this.props.num});
   },
 
@@ -13,9 +14,11 @@ var PriceFilterButton = React.createClass({
       dollarSigns += "$";
     }
 
+    var active = this.props.active ? "active" : "inactive";
+
     return (
       <div className="price-filter-button-container" title={this.props.title}>
-        <a href="#" className="price-filter-button"
+        <a href="#" className={"price-filter-button " + active}
                 onClick={this.handleClick}>
           {dollarSigns}
         </a>
