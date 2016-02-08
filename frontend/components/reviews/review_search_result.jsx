41fe596b89fa85b79c4ebbc7ffb9a7c39c6c2ab8
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-08 18:18:11 -0500

    Fix layout on review search results.

--- frontend/components/reviews/review_search_result.jsx
+++ frontend/components/reviews/review_search_result.jsx
@@ -5,17 +5,22 @@ var ReviewButton = require('./review_button');
 
 var ReviewSearchResult = React.createClass({
   render: function () {
-
     var restaurant = this.props.result;
+
+    var idx = restaurant.address.indexOf(",");
+    var lineOne = restaurant.address.slice(0, idx + 1);
+    var lineTwo = restaurant.address.slice(idx + 2);
+
     var hasReviewed = CurrentUserStore.findReview(restaurant.id);
     var Link = ReactRouter.Link;
 
     return(
-      <li className="group">
+      <li className="search-result group">
         <img className="search-result-thumb" src={restaurant.photo_url}/>
         <Link to={"/restaurants/" + restaurant.id}>{restaurant.name}</Link>
         <img className="stars" src={getStarsUrl(restaurant.mean_rating)} />
-        <p>{restaurant.address}</p>
+        <p>{lineOne}</p>
+        <p>{lineTwo}</p>
         <p>{getPriceRangeString(restaurant.price_range)}</p>
         <ReviewButton
           restaurant={restaurant}
